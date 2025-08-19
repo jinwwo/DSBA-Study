4173e46b8d287bd624e3647d379a409f1c14d0c9
jinwwo <jinwoo_jang@snu.ac.kr>
2025-08-19 07:56:33 +0000

    feat: add gradient accumulation function

--- nlp/main.py
+++ nlp/main.py
@@ -7,13 +7,14 @@ import omegaconf
 import torch
 import torch.nn as nn
 import wandb
+from accelerate import Accelerator
 from omegaconf import OmegaConf
 from tqdm import tqdm
 from transformers import AutoTokenizer, PreTrainedTokenizerBase
 
 from src.data import get_dataloader
 from src.model import EncoderForClassification
-from src.utils import init_wandb, seed_everything, set_device, wandb_login
+from src.utils import seed_everything, set_device, set_wandb
 
 _logger = logging.getLogger("train")
 
@@ -23,6 +24,8 @@ def train_iter(
     inputs: Dict[str, torch.Tensor],
     optimizer: torch.optim.Optimizer,
     device: torch.device,
+    accelerator: Accelerator,
+    step_counter: int
 ) -> Tuple[torch.Tensor, float]:
     """
     Single training iteration (forward, backward, step) and logging.
@@ -46,15 +49,19 @@ def train_iter(
         Accuracy for the batch.
     """
     inputs = {key: value.to(device) for key, value in inputs.items()}
-    logits, loss = model(**inputs)
     
-    optimizer.zero_grad()
-    loss.backward()
-    torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
-    optimizer.step()
+    with accelerator.accumulate(model):
+        logits, loss = model(**inputs)
+        accelerator.backward(loss)
+        
+        if accelerator.sync_gradients:
+            accelerator.clip_grad_norm_(model.parameters(), 1.0)
+            optimizer.step()
+            optimizer.zero_grad()
+            step_counter += 1
+
+    accuracy = calculate_accuracy(logits, inputs["label"])
     
-    accuracy = calculate_accuracy(logits, inputs['label'])
-    wandb.log({'train_loss': loss.item(), 'train_acc': accuracy})
     return loss, accuracy
 
 
@@ -82,10 +89,10 @@ def valid_iter(
     accuracy : float
         Accuracy for the batch.
     """
-    inputs = {key : value.to(device) for key, value in inputs.items()}
+    inputs = {key: value.to(device) for key, value in inputs.items()}
     logits, loss = model(**inputs)
-    
-    accuracy = calculate_accuracy(logits, inputs['label'])
+
+    accuracy = calculate_accuracy(logits, inputs["label"])
     return loss, accuracy
 
 
@@ -133,7 +140,7 @@ def get_model_tokenizer(
     return model.to(device), tokenizer
 
 
-@hydra.main(config_path='configs', config_name='default.yaml')
+@hydra.main(config_path="configs", config_name="default.yaml")
 def main(configs: omegaconf.DictConfig) -> None:
     """
     Entry point for training/validation/testing pipeline.
@@ -141,82 +148,125 @@ def main(configs: omegaconf.DictConfig) -> None:
     print(OmegaConf.to_yaml(configs))
     seed_everything(configs.seed)
     device = set_device(configs.device)
-    
+
     # model & tokenizer
     model, tokenizer = get_model_tokenizer(configs.model, device)
-    
+
     # data loaders
-    train_loader = get_dataloader(configs.data, tokenizer, 'train')
-    valid_loader = get_dataloader(configs.data, tokenizer, 'valid')
-    test_loader = get_dataloader(configs.data, tokenizer, 'test')
-    
+    train_loader = get_dataloader(configs.data, tokenizer, "train")
+    valid_loader = get_dataloader(configs.data, tokenizer, "valid")
+    test_loader = get_dataloader(configs.data, tokenizer, "test")
+
     # set wandb
-    wandb_login(key=configs.wandb_key)
-    init_wandb(model, configs.train)
-    
+    use_wandb = set_wandb(model, configs)
+        
     # save configs and prepare model save path
     model_save_path = configs.train.model_save_path
     os.makedirs(model_save_path, exist_ok=True)
-    OmegaConf.save(config=configs, f=os.path.join(model_save_path, 'configs.yaml'))
-    
+    OmegaConf.save(config=configs, f=os.path.join(model_save_path, "configs.yaml"))
+
     # optimizer & scheduler
-    optimizer = torch.optim.Adam(model.parameters(), lr=configs.train.lr)
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer=optimizer, step_size=1, gamma=0.9)
-    
+    optimizer = torch.optim.AdamW(
+        model.parameters(), lr=configs.train.lr, weight_decay=configs.train.weight_decay
+    )
+    scheduler = torch.optim.lr_scheduler.StepLR(
+        optimizer=optimizer, step_size=1, gamma=0.9
+    )
+
+    # gradient accumulation
+    step_counter = 0
+    accelerator = Accelerator(gradient_accumulation_steps=configs.train.gradient_accumulation_steps)
+    model, optimizer, scheduler, train_loader, valid_loader, test_loader = (
+        accelerator.prepare(
+            model, optimizer, scheduler, train_loader, valid_loader, test_loader
+        )
+    )
+
     # train & validate
     epochs = configs.train.max_epochs
     best_valid_accuracy = -1
     for epoch in range(epochs):
         model.train()
         total_train_loss, total_train_acc = 0.0, 0.0
-        
-        # train
-        for batch in tqdm(train_loader, desc=f'Epoch: {epoch+1}/{epochs}'):            
-            train_loss, train_accuracy = train_iter(model, batch, optimizer, device=device)
 
+        # train
+        for batch in tqdm(train_loader, desc=f"Epoch: {epoch+1}/{epochs}"):
+            train_loss, train_accuracy = train_iter(
+                model, batch, optimizer, device, accelerator, step_counter
+            )
+
+            if use_wandb:
+                wandb.log({"train_loss": train_loss.item(), "train_acc": train_accuracy})
+                
             total_train_loss += train_loss.item()
             total_train_acc += train_accuracy
 
-        _logger.info(f"Epoch {epoch+1}/{epochs} - Train Loss: {total_train_loss / len(train_loader)}")
-        _logger.info(f"Epoch {epoch+1}/{epochs} - Train Acc: {total_train_acc / len(train_loader)}")
-        
+        _logger.info(
+            f"Epoch {epoch+1}/{epochs} - Train Loss: {total_train_loss / len(train_loader)}"
+        )
+        _logger.info(
+            f"Epoch {epoch+1}/{epochs} - Train Acc: {total_train_acc / len(train_loader)}"
+        )
+
         # validate
         total_val_loss, total_val_acc = 0.0, 0.0
-        
+
         model.eval()
         with torch.no_grad():
             for batch in tqdm(valid_loader, desc=f'Validating Epoch" {epoch+1}'):
                 valid_loss, valid_accuracy = valid_iter(model, batch, device)
                 total_val_loss += valid_loss.item()
                 total_val_acc += valid_accuracy
+
+        _logger.info(
+            f"Epoch {epoch+1}/{epochs} - Val Loss: {total_val_loss / len(valid_loader)}"
+        )
+        _logger.info(
+            f"Epoch {epoch+1}/{epochs} - Val Acc: {total_val_acc / len(valid_loader)}"
+        )
         
-        _logger.info(f"Epoch {epoch+1}/{epochs} - Val Loss: {total_val_loss / len(valid_loader)}")
-        _logger.info(f"Epoch {epoch+1}/{epochs} - Val Acc: {total_val_acc / len(valid_loader)}")
-        wandb.log({'val_loss': total_val_loss / len(valid_loader), 'val_acc': total_val_acc / len(valid_loader)})
-        
+        if use_wandb:
+            wandb.log(
+                {
+                    "val_loss": total_val_loss / len(valid_loader),
+                    "val_acc": total_val_acc / len(valid_loader),
+                }
+            )
+
         if total_val_acc > best_valid_accuracy:
-            best_valid_accuracy = total_val_acc            
-            torch.save(model.state_dict(), os.path.join(model_save_path, 'best_model.pt'))
+            best_valid_accuracy = total_val_acc
+            torch.save(
+                model.state_dict(), os.path.join(model_save_path, "best_model.pt")
+            )
             _logger.info(f"Best val Acc: {best_valid_accuracy} updating model...")
             _logger.info(f"Best model updated: Saved at {model_save_path}")
+
+        if use_wandb:
+            wandb.log({"lr": optimizer.param_groups[0]["lr"]})
         
-        wandb.log({'lr': optimizer.param_groups[0]["lr"]})
         scheduler.step()
-        
+
     # test using the best checkpoint
     model.eval()
-    model.load_state_dict(torch.load(os.path.join(model_save_path, 'best_model.pt')))
-    
-    total_test_loss, total_test_acc= 0.0, 0.0
+    model.load_state_dict(torch.load(os.path.join(model_save_path, "best_model.pt")))
+
+    total_test_loss, total_test_acc = 0.0, 0.0
     with torch.no_grad():
         for batch in tqdm(test_loader, desc=f'Testing" {epoch+1}'):
             test_loss, test_accuracy = valid_iter(model, batch, device)
             total_test_loss += test_loss.item()
             total_test_acc += test_accuracy
-            
+
     _logger.info(f"Test Loss: {total_test_loss / len(test_loader)}")
     _logger.info(f"Test Acc: {total_test_acc / len(test_loader)}")
-    wandb.log({'test_loss': total_test_loss / len(test_loader), 'test_acc': total_test_acc / len(test_loader)})
+    
+    if use_wandb:
+        wandb.log(
+            {
+                "test_loss": total_test_loss / len(test_loader),
+                "test_acc": total_test_acc / len(test_loader),
+            }
+        )
 
 
 if __name__ == "__main__":
